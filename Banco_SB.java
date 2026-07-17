fdd610a749db5184267b11cdc4a20ee51bbc4360
  <kevindasilvalima2008@gmail.com>
2026-07-17 13:38:22 -0300

    Sistema de emprestimo

--- Banco_SB.java
+++ Banco_SB.java
@@ -7,6 +7,17 @@ public class Banco_SB {
 
     }
 
+    public static void verificar() throws InterruptedException {
+        while (!sc.hasNextInt()){
+                System.out.println("--------------------");
+                System.out.println("Digite Apenas numeros!");
+                System.out.println("--------------------");
+                sc.next();
+                esperar();
+                continue;
+            }
+    }
+
     public static void espacamento(){
         for (int i = 0; i < 20; i++) {
             System.out.println(" ");
@@ -16,7 +27,7 @@ public class Banco_SB {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws InterruptedException{
         
-        int[] saldo = new int[1];
+        double[] saldo = new double[1];
         saldo[0] = 0;
 
         System.out.println("Bem-vindo ao System Brasil");
@@ -74,6 +85,133 @@ public class Banco_SB {
                     System.out.println(saldo[0]);
                     System.out.println("--------------------");
                     esperar();
+                    
+                    if(saldo[0] == 0){
+                            System.out.println("Nós percebemos que seu saldo esta atualmente em zero, caso voce deseje System Brasil tem os " +
+                                                "menores juros para emprestimos de todo o brasil");
+                            System.out.println("Faca um emprestimo hoje e pague depois tranquilamente");
+                            System.out.println("--------------------");
+                            System.out.println("Deseja fazer um emprestimo?");
+                            System.out.println(" ");
+                            System.out.println("1 - Sim");
+                            System.out.println("2 - Não");
+                            System.out.println("--------------------");
+
+                            verificar();
+
+                            int empres = sc.nextInt();
+                            System.out.println("--------------------");
+                            esperar();
+                            espacamento();
+
+
+                            if (empres == 1){
+                                System.out.println("--------------------");
+                                System.out.println("Digite o valor desejado");
+                                System.out.println("Trabalhamos com juros de 8%");
+                                System.out.println("--------------------");
+                                verificar();
+
+                                double money = sc.nextDouble();
+                                double juross = money * 0.08;
+
+                                System.out.println("Voce fez um emprestimo de R$: " + money);
+                                System.out.println("Total a Pagar: " + money + " + " + juross);
+
+                                saldo[0] = saldo[0] + money;
+                                double[] saldoj = new double[1];
+                                saldoj[0] = saldo[0] + juross;
+
+                                boolean loop = true;
+
+                                while(loop){
+                                    System.out.println("--------------------");
+                                    System.out.println("Escolha suas Parcelas: ");
+                                    System.out.println("1x " + saldoj[0]);
+                                    System.out.println("2x " + saldoj[0] / 2);
+                                    System.out.println("3x " + saldoj[0] / 3);
+                                    System.out.println("4x " + saldoj[0] / 4);
+                                    System.out.println("5x " + saldoj[0] / 5);
+                                    System.out.println("6x " + saldoj[0] / 6);
+                                    System.out.println("Caso deseje cancelar essa acão digite 0");
+                                    System.out.println("--------------------");
+
+                                    verificar();
+                                    int parc = sc.nextInt();
+                                    
+                                    if (parc == 1){
+                                        System.out.println("Voce escolher pagar em " + 1 + " parcelas");
+                                        System.out.println("Valor a pagar no proximo mes" + saldoj[0]);
+                                        System.out.println("--------------------");
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc == 2){
+                                        System.out.println("Voce escolher pagar em " + 2 + " parcelas");
+                                        System.out.println("Valor a pagar mensalmente " + saldoj[0] / 2);
+                                        System.out.println("--------------------");
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc == 3){
+                                        System.out.println("Voce escolher pagar em " + 2 + " parcelas");
+                                        System.out.println("Valor a pagar mensalmente " + saldoj[0] / 2);
+                                        System.out.println("--------------------");
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc == 4){
+                                        System.out.println("Voce escolher pagar em " + 2 + " parcelas");
+                                        System.out.println("Valor a pagar mensalmente " + saldoj[0] / 2);
+                                        System.out.println("--------------------");
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc == 5){
+                                        System.out.println("Voce escolher pagar em " + 2 + " parcelas");
+                                        System.out.println("Valor a pagar mensalmente " + saldoj[0] / 2);
+                                        System.out.println("--------------------");
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc == 6){
+                                        System.out.println("Voce escolher pagar em " + 2 + " parcelas");
+                                        System.out.println("Valor a pagar mensalmente " + saldoj[0] / 2);
+                                        System.out.println("--------------------");
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc == 0){
+                                        System.out.println("Voce cancelou essa acão retornando ao menu");
+                                        System.out.println("--------------------");
+                                        saldo[0] = saldo[0] - money;
+                                        esperar();
+                                        espacamento();
+                                        loop = false;
+                                    }
+                                    if (parc > 6){
+                                        System.out.println("--------------------");
+                                        System.out.println("Digite apenas o valor das parcelas disponiveis");
+                                        esperar();
+                                        espacamento();
+                                        continue;
+                                    }
+                                    if (parc < 0){
+                                        System.out.println("--------------------");
+                                        System.out.println("Digite apenas o valor das parcelas disponiveis");
+                                        esperar();
+                                        espacamento();
+                                        continue;
+                                    }
+                                }
+                            }
+                        }
 
                     break;
                 case 2: 
@@ -114,13 +252,17 @@ public class Banco_SB {
                         System.out.println("Quantidade desejada indisponivel (saldo insuficiente!)");
                         System.out.println("Seu saldo é de: " + saldo[0]);
                         System.out.println("--------------------");
-                        esperar();
                         espacamento();
+                        System.out.println("--------------------");
+
+                        
                     }
 
                     if (saldo[0] <= -1){
                         saldo[0] = 0;
                     }
+                    
+                    
                     break;
 
                 case 3:
